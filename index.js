8f6001076bbde4b5b570a685a9ead6c673579c09
Anthony Speece <anthonyspeece@gmail.com>
2022-10-22 12:55:56 -0500

    adding functionality of project

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const inquirer = require('inquirer')
-const newEmployees = []
-const Manager = require('./lib/Manager')
+const inquirer = require("inquirer");
+const newEmployees = [];
+const Manager = require("./lib/Manager");
 
-function createMananger(){
-    inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "What is the Manager's Name "
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: "What is the Manager's ID "
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: "What is the Manager's Email "
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: "What is the Manager's office number "
-        },
-        
-    ]).then((answers)=>{
-        const newManager = new Manager(answer.managerName, answers.managerId,
-            answers.managerEmail, answers.managerOfficeNumber)
-            newEmployees.push(newManager)
-    })
+function createMananger() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the Manager's Name ",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the Manager's ID ",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the Manager's Email ",
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the Manager's office number ",
+      },
+    ])
+    .then((answers) => {
+      const newManager = new Manager(
+        answer.managerName,
+        answers.managerId,
+        answers.managerEmail,
+        answers.managerOfficeNumber
+      );
+      newEmployees.push(newManager);
+    });
 }
